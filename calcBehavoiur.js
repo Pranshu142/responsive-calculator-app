7759d55cd3c535c436d6f04f32c8036ce2df50ef
Pranshu Pandey <pranshupandey232@gmail.com>
2024-05-30 18:18:43 +0530

    change some functionality

--- calcBehavoiur.js
+++ calcBehavoiur.js
@@ -2,57 +2,65 @@ document.addEventListener("DOMContentLoaded", () => {
     // Convert HTMLCollection to an array using Array.from
     const btns = Array.from(document.getElementsByClassName("calcBtn"));
     const display = document.getElementById("inp");
-    let parser = (res) => {
-        if (res == Infinity || isNaN(res)) {
-            display.value = "Can't calculate !!";
-            return undefined;
-        }
-        else return res;
-    }
 
-    display.addEventListener("keypress", (e) => {
+    const clearErrorMessages = () => {
         if (
             display.value === "Enter a valid expression" ||
             display.value === "Can't calculate !!"
         ) {
             display.value = "";
         }
+    };
+
+    const parser = (res) => {
+        if (res === Infinity || isNaN(res)) {
+            display.value = "Can't calculate !!";
+            return undefined;
+        } else {
+            return res;
+        }
+    };
+
+    const evaluateExpression = () => {
         try {
-
-            if (e.key === "Enter")
-
-                if (parser(eval(display.value)) != undefined)
-                    display.value = parser(eval(display.value));
+            const result = parser(eval(display.value));
+            if (result !== undefined) {
+                display.value = result;
+            }
         } catch (e) {
             display.value = "Enter a valid expression";
+        }
+    };
+
+    display.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+            clearErrorMessages();
+            evaluateExpression();
         }
     });
 
     btns.forEach((element) => {
         element.addEventListener("click", (e) => {
-            console.log(e);
-            console.log(e.target.innerText);
-            let val = e.target.innerText;
-            if (
-                display.value === "Enter a valid expression" ||
-                display.value === "Can't calculate !!"
-            ) {
+            const val = e.target.innerText;
+            clearErrorMessages();
+
+            if (val === "C") {
                 display.value = "";
+            } else if (val === "B") {
+                display.value = display.value.slice(0, -1);
+            } else if (val === "=") {
+                evaluateExpression();
+            } else {
+                display.value += val;
             }
-            if (val != "B" && val != "C" && val != "=") display.value += val;
-            if (val === "C") display.value = "";
-            if (val === "B") {
-                let first = display.value;
-                display.value = first.substring(0, first.length - 1);
-            }
-            if (val === "=") {
-                try {
-                    if (parser(eval(display.value)) != undefined)
-                        display.value = parser(eval(display.value));
-                } catch (e) {
-                    display.value = "Enter a valid expression";
-                }
-            }
+        });
+
+        // Adding visual feedback for button presses
+        element.addEventListener("mousedown", () => {
+            element.classList.add("active");
+        });
+        element.addEventListener("mouseup", () => {
+            element.classList.remove("active");
         });
     });
 });
